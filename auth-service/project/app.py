dccbc658217aa28a9d01ba79f1aecb9b8b9a0729
Matt1479 <gitmatt1479@gmail.com>
2025-12-12 15:12:15 +0100

    Feature(auth-service/project/app.py): Implement JWT authentication.

--- auth-service/project/app.py
+++ auth-service/project/app.py
@@ -1,5 +1,5 @@
-import requests
 from flask import Flask, jsonify, request, make_response
+import jwt
 import json
 import os
 
@@ -13,9 +13,29 @@ with open("./project/users.json", "r") as f:
     users = json.load(f)
 
 
-@app.route("/api/")
+@app.route("/api/auth")
 def index():
-    return "Hello, this is Auth Service"
+    return jsonify({"message": "Hello, this is Auth Service"})
+
+
+@app.route("/api/auth/login", methods=["POST"])
+def login():
+    if not request.is_json:
+        return jsonify({"error": "Unsupported Media Type"}), 415
+    
+    data = request.get_json(silent=True)
+    if data is None:
+        return jsonify({"error": "Invalid or missing JSON body"}), 400
+    
+    username = data.get("username")
+    password = data.get("password")
+    for user in users:
+        if user["username"] == username and user["password"] == password:
+            token = jwt.encode({"user_id": user["id"]}, app.config["SECRET_KEY"], algorithm="HS256")
+            response = make_response(jsonify({"message": "Authentication successful"}))
+            response.set_cookie("token", token)
+            return response, 200
+    return jsonify({"error": "Invalid username or password"}), 401
 
 
 if __name__ == "__main__":
