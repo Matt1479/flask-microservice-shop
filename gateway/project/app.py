d99006331242e51e93400915d8b681676cc8e274
Matt1479 <gitmatt1479@gmail.com>
2025-12-13 17:12:30 +0100

    Refactor(gateway): Use container names (soon-to-be) defined in docker-compose.yml as hostnames in gateway.

--- gateway/project/app.py
+++ gateway/project/app.py
@@ -9,9 +9,9 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 port = int(os.environ.get("PORT", 5000))
 
 SERVICE_ROUTES = {
-    "/api/auth": "http://localhost:5001",
-    "/api/shop": "http://localhost:5002",
-    "/api/logs": "http://localhost:5003"
+    "/api/auth": "http://auth-service:5001",
+    "/api/shop": "http://shop-service:5002",
+    "/api/logs": "http://logs-service:5003"
 }
 PUBLIC_ROUTES = [
     "/api/auth"
