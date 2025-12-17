275d175ac656a57e3e9660d8d5e1d4f298177576
Matt1479 <gitmatt1479@gmail.com>
2025-12-17 14:30:56 +0100

    Refactor/feature(logs, project/app.py): Use ValueError instead of Exception. Define fields constants. Improve logging and add more fields.

--- logs/project/app.py
+++ logs/project/app.py
@@ -1,12 +1,17 @@
 import os
 import utils
+
 from datetime import datetime
 from flask import Flask, jsonify, request
 from typing import Any
+from urllib.parse import urlencode
 
 
 app = Flask(__name__)
 port = int(os.environ.get("PORT", 5003))
+
+REQUIRED_FIELDS = ["method", "endpoint"]
+OPTIONAL_FIELD = "payload"
 
 LOGS: list[dict[str, Any]] = []
 
@@ -15,22 +20,36 @@ LOGS: list[dict[str, Any]] = []
 def logs():
     try:
         user_id = utils.require_user_id()
-        utils.require_admin_role()
-    except Exception as e:
-        return jsonify({"error": e.args[0]}), e.args[1]
+        user_role = utils.require_role()
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 401
 
     if request.method == "POST":
-        data = request.get_json(silent=True)
+        data: dict[str, Any] | Any | None = request.get_json(silent=True)
+
         if data is None:
             return jsonify({"error": "Invalid or missing JSON body"}), 400
         
-        if not data.get("action"):
-            return jsonify({"error": "Missing required field: 'action'"}), 400
-        
-        data["id"] = max([log["id"] for log in LOGS]) + 1 if LOGS else 1
-        data["timestamp"] = datetime.now()
-        data["user_id"] = int(user_id)
-        LOGS.append(data)
+        log = {
+            "id": max([log["id"] for log in LOGS]) + 1 if LOGS else 1,
+            "timestamp": datetime.now(),
+            "user_id": int(user_id),
+            "user_role": user_role
+        }
+
+        for field in REQUIRED_FIELDS:
+            if field not in data:
+                return jsonify({"error": f"{field} is required"}), 400
+            log[field] = data[field]
+
+        if OPTIONAL_FIELD in data:
+            try:
+                log[OPTIONAL_FIELD] = urlencode(data[OPTIONAL_FIELD])
+            except TypeError as e:
+                return jsonify({"error": str(e)}), 400
+
+        LOGS.append(log)
+
         return jsonify({"message": "Log added successfully"}), 200
     else:
         # GET
