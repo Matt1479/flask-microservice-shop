777e30ae9b5f062335e480bc56ccb1012b5ad6b5
Matt1479 <gitmatt1479@gmail.com>
2025-12-17 14:29:36 +0100

    Refactor(logs, project/utils.py): Use ValueError instead of Exception, require_admin_role -> require_role.

--- logs/project/utils.py
+++ logs/project/utils.py
@@ -12,18 +12,15 @@ def require_user_id():
     user_id = request.headers.get("X-User-Id")
 
     if not user_id:
-        raise Exception("user id is required", 401)
+        raise ValueError("user id is required")
 
     return user_id
 
 
-def require_admin_role():
+def require_role():
     user_role = request.headers.get("X-User-Role")
 
     if not user_role:
-        raise Exception("user role is required", 401)
-
-    if user_role != "admin":
-        raise Exception("You have insufficient rights to access this resource", 403)
+        raise ValueError("user role is required")
 
     return user_role
